4439d8f48c724245a5e2d8774744e6b0f4e80a05
francesca <fradeluca85@gmail.com>
2021-11-28 10:50:18 +0100

    aggiunta didascalia

--- js/main.js
+++ js/main.js
@@ -13,11 +13,31 @@ const app = new Vue({
     el:'#root',
     data:{
         images:[
-            "./img/01.jpg",
-            "./img/02.jpg",
-            "./img/03.jpg",
-            "./img/04.jpg",
-            "./img/05.jpg",
+            {
+                titolo: 'Svezia',
+                testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                img: 'img/01.jpg'
+            },
+            {
+                titolo: 'Svizzera',
+                testo: 'Lorem ipsum',
+                img: 'img/02.jpg'
+            },
+            {
+                titolo: 'Gran Bretagna',
+                testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                img: 'img/03.jpg'
+            },
+            {
+                titolo: 'Germania',
+                testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                img: 'img/04.jpg'
+            },
+            {
+                titolo: 'Paradise',
+                testo: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                img: 'img/05.jpg'
+            },
         ],
         currentImg:0
     },
